027c0e580791798d3e77eca0e100308cf8b90fe0
maxim <thisblack.jack@mail.ru>
2024-02-26 00:14:01 +0300

    symmetric difference of sets and prime numbers

--- main.py
+++ main.py
@@ -5,28 +5,24 @@ def generate_random_list() -> list:
     return [random.randint(1, 100) for _ in range(10)]
 
 
-def get_even_numbers(numbers) -> list:
-    even_numbers = []
-    for number in numbers:
-        if number % 2 == 0:
-            even_numbers.append(number)
-    return even_numbers
+def sum_numbers(numbers) -> int:
+    return sum(numbers)
 
 
-def get_common_numbers(list1, list2) -> set:
-    return set(list1 + list2)
+def get_unique_numbers(list1, list2) -> set:
+    return set(list1) ^ set(list2)  # Симметрическая разность множеств
 
 
-def is_even_or_odd(number) -> str:
-    if number % 2 == 0:
-        return 'even'
-    return 'odd'
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
 if __name__ == '__main__':
     random_numbers = generate_random_list()
-    print(get_even_numbers(random_numbers))
-    print(f'{random_numbers[0]}: {is_even_or_odd(random_numbers[0])}')
-    random_list1 = generate_random_list()
-    random_list2 = generate_random_list()
-    print(get_common_numbers(random_list1, random_list2))
+    print(f'Is {random_numbers[0]} prime? {is_prime(random_numbers[0])}')
+    print(f'The sum of list:\n{random_numbers}\nis: {sum_numbers(random_numbers)}')
