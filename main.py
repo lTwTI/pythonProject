a3364a7220361e1809a6f3b98938689430c98702
maxim <thisblack.jack@mail.ru>
2024-02-28 13:10:35 +0300

    aliens invasion... again...

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import random
-
-
-def generate_random_list() -> list:
-    return [random.randint(1, 100) for _ in range(10)]
-
-
-def sum_numbers(numbers) -> int:
-    return sum(numbers)
-
-
-def get_unique_numbers(list1, list2) -> set:
-    return set(list1) ^ set(list2)  # Симметрическая разность множеств
-
-
-def is_prime(number):
-    if number < 2:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
-
-
 if __name__ == '__main__':
-    random_numbers = generate_random_list()
-    print(f'Is {random_numbers[0]} prime? {is_prime(random_numbers[0])}')
-    print(f'The sum of list:\n{random_numbers}\nis: {sum_numbers(random_numbers)}')
+    aliens = []
+    for alien_number in range(30):
+        new_alien = {'color': 'green', 'speed': 'fast', 'points': 7, 'number': alien_number}
+        aliens.append(new_alien)
+    for alien in aliens[:5]:
+        print(alien)
+    print('...')
+    print(f'Total amount of aliens is {len(aliens)}')
