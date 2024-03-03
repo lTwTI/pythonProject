8b2e4ea9d179f7c3ddbb9a61228582a97cd1f981
maxim <thisblack.jack@mail.ru>
2024-03-03 12:55:40 +0300

    feature: using and distribute loading module

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import time
-
-
-def loader(info, timer) -> None:
-    chars = '|/-\\'
-    pointer = 0
-
-    end_time = time.time() + timer
-
-    print(info)
-
-    while time.time() < end_time:
-        remaining_time = int(end_time - time.time())
-        current_char = chars[pointer]
-        print(f'\rLoading: {current_char} {remaining_time}s left', end='', flush=True)
-        pointer = (pointer + 1) % len(chars)
-        time.sleep(0.25)
-
-    print('\rThe download process has been successfully completed.')
+import loading
 
 
 if __name__ == '__main__':
     message = 'Take a coffee break.'
-    loader(message, 5)
+    loading.loader(message, 5)
