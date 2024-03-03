2418dc99fa476ab3b27fa28ecabfb038bc1166bf
maxim <thisblack.jack@mail.ru>
2024-03-03 19:50:58 +0300

    Car super class

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-import loading
+class Car:
+    def __init__(self, manufacturer, model, year):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f'{self.year} {self.manufacturer.title()} {self.model.title()}'
+        return long_name
+
+    def read_odometer(self):
+        print(f'This car has {self.odometer_reading} miles on it.')
+
+    def increment_odometer(self, miles):
+        if miles >= 0:
+            self.odometer_reading += miles
+        else:
+            print("You can't roll back the odometer!")
 
 
 if __name__ == '__main__':
-    message = 'Take a coffee break.'
-    loading.loader(message, 5)
+    print()
